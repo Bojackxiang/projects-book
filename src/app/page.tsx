f22e437df6eabdcff33bc51f08acf5a8bb4cc72a
Alex <alex@q-83.com>
2023-06-25 19:35:50 +1000

    header-and-footer: update header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      hello world
-    </div>
+    <section className='flex-start flex-col paddings mb-16'>
+      <h1 className='text-red-500'>Categories</h1>
+      <h1>posts</h1>
+      <h1>load more</h1>
+    </section>
   )
 }
 
